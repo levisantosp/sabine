c70dcecb7ae2febbdca4f5434ec217527359e8ae
levispires <contatowizard01@gmail.com>
2024-05-01 14:27:14 -0300

    updated the player command

--- app/commands/player.js
+++ app/commands/player.js
@@ -16,34 +16,19 @@ export default class PlayerCommand extends Command {
     })).json()).data.filter(p => p.name.toLowerCase() === ctx.args.slice(0).join(' ').toLowerCase())
     if(players.length === 0) return ctx.reply('commands.player.no_player_found')
     
+    const player = players[0]
+    const p = (await (await fetch(`https://vlr.orlandomm.net/api/v1/players/${player.id}`, {
+      method: 'GET'
+    })).json()).data
     const embed = new EmbedBuilder()
-    if (players.length === 1) {
-      for(const player of players) {
-        const p = (await (await fetch(`https://vlr.orlandomm.net/api/v1/players/${player.id}`, {
-          method: 'GET'
-        })).json()).data
-        embed.setTitle(`:flag_${p.info.flag}: ${p.info.user}`)
-        embed.setThumbnail(p.info.img)
-        embed.setDescription(await this.locale('commands.player.embed.desc', {
-          name: p.info.name,
-          team: `[${p.team.name}](${p.team.url})`,
-          pt: p.pastTeams.map(t => `[${t.name}](${t.url})`).join(', ')
-        }))
-      }
-    }
-    else {
-      embed.setDescription(await this.locale('commands.player.embed.desc2', {
-        q: players.length,
-        s: ctx.args.slice(0).join(' ')
-      }))
-      for(const player of players) {
-        embed.addField(`:flag_${player.country}: ${player.name}`, await this.locale('commands.player.embed.field', {
-          id: player.id,
-          t: player.teamTag,
-          p: player.url
-        }))
-      }
-    }
+    .setTitle(`:flag_${p.info.flag}: ${p.info.user}`)
+    .setThumbnail(p.info.img)
+    .setDescription(await this.locale('commands.player.embed.desc', {
+      name: p.info.name,
+      team: `[${p.team.name}](${p.team.url})`,
+      pt: p.pastTeams.map(t => `[${t.name}](${t.url})`).join(', '),
+      lt: `[${p.results[0].teams[0].name} ${p.results[0].teams[0].points}-${p.results[0].teams[1].points} ${p.results[0].teams[1].name}](${p.results[0].match.url})`
+    }))
     ctx.reply(embed.build())
   }
 }
