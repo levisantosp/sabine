f510cb8b656e8d2b49a80c3c43638ee2cfde5097
levispires <contatowizard01@gmail.com>
2024-05-06 12:57:48 -0300

    implement caching system

--- app/commands/TeamCommand.js
+++ app/commands/TeamCommand.js
@@ -1,5 +1,10 @@
 import Command from '../structures/command/Command.js'
 import EmbedBuilder from '../structures/embed/EmbedBuilder.js'
+const teamsCached = {}
+const teamsAPICache = await (await fetch('https://vlr.orlandomm.net/api/v1/teams?limit=all', {
+  method: 'GET'
+})).json()
+const teamAPICache = {}
 
 export default class TeamCommand extends Command {
   constructor(client) {
@@ -22,23 +27,34 @@ export default class TeamCommand extends Command {
   async run(ctx) {
     let team = ctx.args.slice(0).join(' ')
     if (!team) return ctx.reply('commands.team.insert_team')
-    const teams = (await (await fetch('https://vlr.orlandomm.net/api/v1/teams?limit=all', {
-      method: 'GET'
-    })).json()).data.filter(t => t.name.toLowerCase() === team.toLowerCase())
-    if (teams.length === 0) return ctx.reply('commands.team.no_team_found')
-    
-    team = teams[0]
-    const t = (await (await fetch(`https://vlr.orlandomm.net/api/v1/teams/${team.id}`, {
-      method: 'GET'
-    })).json()).data
+    if(!teamAPICache[team]) {
+      teamAPICache[team] = teamsAPICache.data.find(t => t.name.toLowerCase() === team.toLowerCase())
+    }
+
+    const teamAPI = teamAPICache[team]
+    if(!teamAPI) return ctx.reply('commands.team.no_team_found')
+    let teamCache = teamsCached[teamAPI.id]
+    if(!teamCache) {
+      const t = (await (await fetch(`https://vlr.orlandomm.net/api/v1/teams/${teamAPI.id}`, {
+        method: 'GET'
+      })).json()).data
+      teamCache = teamsCached[t.id] = {
+        players: t.players.map(p => `[${p.user}](${p.url})`).join(', '),
+        staff: t.staff.map(s => `[${s.user}](${s.url})`).join(', '),
+        lastMatch: `[${t.results[0].teams[0].name} ${t.results[0].teams[0].points}-${t.results[0].teams[1].points} ${t.results[0].teams[1].name}](${t.results[0].match.url})`,
+        nextMatch: `[${t.upcoming[0]?.teams[0].name} x ${t.upcoming[0]?.teams[1].name}](${t.upcoming[0]?.match.url})`,
+        title: `${t.info.name} (${t.info.tag})`,
+        logo: t.info.logo
+      }
+    }
     const embed = new EmbedBuilder()
-    .setTitle(`${t.info.name} (${t.info.tag})`)
-    .setThumbnail(t.info.logo)
+    .setTitle(teamCache.title)
+    .setThumbnail(teamCache.logo)
     .setDescription(await this.locale('commands.team.embed.desc', {
-      p: t.players.map(p => `[${p.user}](${p.url})`).join(', '),
-      s: t.staff.map(s => `[${s.user}](${s.url})`).join(', '),
-      lt: `[${t.results[0].teams[0].name} ${t.results[0].teams[0].points}-${t.results[0].teams[1].points} ${t.results[0].teams[1].name}](${t.results[0].match.url})`,
-      n: `[${t.upcoming[0]?.teams[0].name} x ${t.upcoming[0]?.teams[1].name}](${t.upcoming[0]?.match.url})`
+      p: teamCache.players,
+      s: teamCache.staff,
+      lt: teamCache.lastMatch,
+      n: teamCache.nextMatch
     }))
     ctx.reply(embed.build())
   }
