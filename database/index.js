93e527e1dfe59c177b13fe4e516ecdecf61bed3c
levispires <contatowizard01@gmail.com>
2024-05-26 21:42:32 -0300

    importing `Matches.js`

--- database/index.js
+++ database/index.js
@@ -1,9 +1,11 @@
 import User from './schemas/User.js'
 import Guild from './schemas/Guild.js'
 import Client from './schemas/Client.js'
+import Matches from './schemas/Matches.js'
 
 export {
   User,
   Guild,
-  Client
+  Client,
+  Matches
 }
