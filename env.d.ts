b4de91c5f4702cbd718d46ce996dacc6bf07f341
levispires <contatowizard01@gmail.com>
2024-07-07 15:01:30 -0300

    .

--- env.d.ts
+++ env.d.ts
@@ -1,12 +1,13 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      BOT_TOKEN: string,
-      MONGO_URI: string,
-      PREFIX: string,
-      ERROR_LOG: string,
-      COMMAND_LOG: string,
-      GUILDS_LOG: string,
+      BOT_TOKEN: string
+      MONGO_URI: string
+      PREFIX: string
+      ERROR_LOG: string
+      COMMAND_LOG: string
+      GUILDS_LOG: string
+      INTERVAL: number
     }
   }
 }
