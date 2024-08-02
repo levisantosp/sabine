c3427b78721a72558a06f14df2761313283d8e08
levispires <contatowizard01@gmail.com>
2024-08-01 22:39:03 -0300

    add a main controller for all controllers

--- src/scraper/index.ts
+++ src/scraper/index.ts
@@ -4,10 +4,26 @@ import PlayersController from './controllers/PlayersController'
 import ResultsController from './controllers/ResultsController'
 import TeamsController from './controllers/TeamsController'
 
-export {
-  EventsController,
-  MatchesController,
-  PlayersController,
-  ResultsController,
-  TeamsController
+export default class MainController {
+  static async getEvents() {
+    return await EventsController.get()
+  }
+  static async getMatches() {
+    return await MatchesController.get()
+  }
+  static async getAllPlayers() {
+    return await PlayersController.get()
+  }
+  static async getPlayerById(id: string | number) {
+    return await PlayersController.getById(id)
+  }
+  static async getResults() {
+    return await ResultsController.get()
+  }
+  static async getAllTeams() {
+    return await TeamsController.get()
+  }
+  static async getTeam(id: string | number) {
+    return await TeamsController.getById(id)
+  }
 }
