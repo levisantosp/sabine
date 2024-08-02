0d7111f6e4cf5545eb89a693da5556bfac98a11c
levispires <contatowizard01@gmail.com>
2024-08-01 22:32:04 -0300

    fix, add, and remove types

--- types/index.d.ts
+++ types/index.d.ts
@@ -28,183 +28,100 @@ type CommandStructure = {
   }
   options?: SlashOptions[]
 }
-type Team = {
+type TeamResponse = {
+  status: string
+  data: TeamData
+}
+type EventsData = {
+  id?: string
   name: string
-  country: string
-  score: string
-  won?: boolean
+  status?: string
+  image: string
+  url: string
+}
+type MatchTeam = {
+  name: string
+  country?: string
+}
+type MatchTournament = {
+  name: string
+  image: string
 }
 type MatchesData = {
-  id: string
-  teams: Team[]
+  id?: string
+  teams: MatchTeam[]
   status: string
-  event: string
-  tournament: string
-  img: string
-  in: string
+  tournament: MatchTournament
+  stage: string
+  when: number
 }
-type TMatches = {
-  status: string
-  size: number
-  data: MatchesData[]
-}
-type ResultsData = {
-  id: string
-  teams: Team[]
-  status: string
-  ago: string
-  event: string
-  tournament: string
-  img: string
-}
-type Results = {
-  status: string
-  size: number
-  data: ResultsData[]
-}
-// type AutocompleteInteractionDataOptions = {
-//   value: string
-//   type: 3
-//   name: string
-//   focused: boolean
-// }
-// type AutocompleteInteractionOptions = {
-//   type: 1
-//   options: AutocompleteInteractionDataOptions[]
-//   name: string
-//   value: string
-// }
-// type AutocompleteInteractionData = {
-//   name: string
-//   options: AutocompleteInteractionOptions[]
-//   id: string
-// }
-type TournamentData = {
-  id: string
-  name: string
-  status: 'ongoing' | 'completed' | 'upcoming'
-  prizepool: string
-  dates: string
-  country: string
-  img: string
-}
-type Tournament = {
-  status: string
-  size: number
-  data: TournamentData[]
-}
-type PlayerPagination = {
-  page: number
-  limit: number
-  totalElements: number
-  totalPages: number
-  hasNextPage: boolean
-}
-type PlayerData = {
-  id: string
-  url: string
+type PlayersData = {
   name: string
   teamTag: string
   country: string
-}
-type Player = {
-  status: string
-  pagination: PlayerPagination
-  data: PlayerData[]
-}
-type PlayerTeamData = {
   id: string
-  url: string
+}
+type PlayerCountry = {
   name: string
-  logo: string
-  joined: string
-}
-type PlayerResultsMatchData = {
-  id: string
-  url: string
-}
-type PlayerResultsEventData = {
-  name: string
-  logo: string
-}
-type PlayerResultsTeamData = {
-  name: string
-  tag: string
-  logo: string
-  points: string
-}
-type PlayerResultsData = {
-  match: PlayerResultsMatchData
-  event: PlayerResultsEventData
-  teams: PlayerResultsTeamData[]
-}
-type PlayerInfo = {
-  id: string
-  url: string
-  img: string
-  user: string
-  name: string
-  country: string
   flag: string
 }
 type PlayerPastTeam = {
   id: string
   url: string
   name: string
-  logo: string
-  info: string
 }
-type PlayerResData = {
-  info: PlayerInfo
-  team: PlayerTeamData
-  results: PlayerResultsData[]
-  pastTeams: PlayerPastTeam[]
-}
-type PlayerRes = {
-  status: string
-  data: PlayerResData
-}
-type TeamResData = {
-  id: string
-  url: string
+type PlayerLastResultTeam = {
   name: string
-  img: string
+  score: string
+}
+type PlayerLastResult = {
+  id: string
+  teams: PlayerLastResultTeam[]
+  url: string
+}
+type PlayerData = {
+  avatar: string
+  user: string
+  realName: string
+  country: PlayerCountry
+  currentTeam: string
+  pastTeams: PlayerPastTeam[]
+  lastResults: PlayerLastResult[]
+}
+type ResultsTeam = {
+  name: string
+  score: string
+  country: string
+  winner: boolean
+}
+type ResultsData = {
+  id: string
+  teams: ResultsTeam[]
+  status: string
+  tournament: MatchTournament
+  stage: string
+  when: number
+}
+type TeamsData = {
+  id: string
+  name: string
+  url?: string
+  image?: string
   country: string
 }
-type TeamRes = {
-  status: string
-  region: string
-  size: number
-  data: TeamResData[]
+type Roster = {
+  id: string
+  user: string
+  url: string
 }
-type TeamInfo = {
-  name: string
-  tag: string
-  logo: string
-}
-type TeamUpcomingMatch = {
-  match: {
-    id: string
-    url: string
-  }
-  event: {
-    name: string
-    logo: string
-  }
-  teams: Array<{
-    name: string
-    tag: string
-    logo: string
-  }>
+type TeamRoster = {
+  players: Roster
+  staffs: Roster
 }
 type TeamData = {
-  info: TeamInfo
-  players: PlayerInfo[]
-  staff: PlayerInfo[]
-  results: PlayerResultsData[]
-  upcoming: TeamUpcomingMatch[]
-}
-type TeamResponse = {
-  status: string
-  data: TeamData
+  id: string
+  name: string
+  tag: string
+  roster: TeamRoster
+  lastResults: PlayerLastResult[]
 }
