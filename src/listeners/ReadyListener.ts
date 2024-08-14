3067614596fd669bce5e00780c0287b5ff6d7568
levispires <contatowizard01@gmail.com>
2024-08-14 10:57:45 -0300

    fix(error): DiscordRESTError: Missing Permissions on POST /api/v10/channels/1270464818805080115/messages

--- src/listeners/ReadyListener.ts
+++ src/listeners/ReadyListener.ts
@@ -89,6 +89,7 @@ export default class ReadyListener extends Listener {
                     }
                   ]
                 })
+                .catch(() => {})
               }
             }
           }
