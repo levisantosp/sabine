449ec3fea001c0f8d407173142d8c13d4e32fe59
levispires <contatowizard01@gmail.com>
2024-09-01 15:13:12 -0300

    test

--- src/listeners/ReadyListener.ts
+++ src/listeners/ReadyListener.ts
@@ -137,8 +137,8 @@ export default class ReadyListener extends Listener {
         }
         guild.matches = []
         let data = res.filter(d => guild.events.some(e => e.name === d.tournament.name))
-        console.log(data)
         for(const e of guild.events) {
+          console.log('teste')
           if(!this.client.getChannel(e.channel1)) continue
           let messages = await this.client.rest.channels.getMessages(e.channel1)
           await this.client.rest.channels.deleteMessages(e.channel1, messages.map(m => m.id), 'Removing completed matches.')
