b23e1a3e9c1bcfd6daa0ad2c93c4cb3b57999cc3
levisantosp <contatowizard01@gmail.com>
2025-03-17 19:40:50 -0300

    minor changes

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -120,8 +120,8 @@ export const helper = {
   privacy: "Before you start to use the bot, you must accept the [Terms of Service and Privacy](https://levispires.github.io/sabine-terms/)",
   verified: "<:success:1300882212190945292> Make your prediction by clicking the button below.",
   pickem: {
-    label: "Pick\"em",
-    res: "Join the PICK\"EM for a chance to win a <:booster:1272968894239215636> **Discord Nitro** or a <:nitro:1272968817496297542> **Discord Nitro Basic** by joining our official server!\nhttps://discord.gg/g5nmc376yh"
+    label: "Pick'em",
+    res: "Join the PICK\'EM for a chance to win a <:booster:1272968894239215636> **Discord Nitro** or a <:nitro:1272968817496297542> **Discord Nitro Basic** by joining our official server!\nhttps://discord.gg/g5nmc376yh"
   },
   banned: "<:error:1300882259078938685> **You are banned and can not use the bot.\n<:warn:869393116854108191> If you think this is a mistake, contact us in our Discord server!**\n**When:** {when}\n**Ends at:** {ends}\n**Reason:** `{reason}`",
   interaction_failed: "<:error:1300882259078938685> It was not possible to run this action... If the problem persists, report it to the team in my [support server](https://discord.gg/g5nmc376yh).",
