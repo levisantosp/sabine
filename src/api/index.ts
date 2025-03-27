7a2be94e37ec9ea80f3f627ddb18304c95e8cacd
levisantosp <contatowizard01@gmail.com>
2025-03-27 17:48:41 -0300

    added league of legends services

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,17 +3,26 @@ import ValorantMatches from "./services/valorant/MatchesService.js"
 import ValorantPlayers from "./services/valorant/PlayersService.js"
 import ValorantTeams from "./services/valorant/TeamsService.js"
 import ValorantResults from "./services/valorant/ResultsService.js"
+import LOLEvents from "./services/lol/EventsService.js"
+import LOLMatches from "./services/lol/MatchesService.js"
+import LOLResults from "./services/lol/ResultsService.js"
 
 export default class Service {
   private __auth: string;
   public constructor(auth: string) {
     this.__auth = auth;
   }
-  public async getEvents() {
-    return await ValorantEvents.get(this.__auth);
+  public async getEvents(game: "valorant" | "lol") {
+    if(game === "valorant") {
+      return await ValorantEvents.get(this.__auth);
+    }
+    else return await LOLEvents.get(this.__auth);
   }
-  public async getMatches() {
-    return await ValorantMatches.get(this.__auth);
+  public async getMatches(game: "valorant" | "lol") {
+    if(game === "valorant") {
+      return await ValorantMatches.get(this.__auth);
+    }
+    else return await LOLMatches.get(this.__auth);
   }
   public async getAllPlayers() {
     return await ValorantPlayers.get(this.__auth);
@@ -27,7 +36,10 @@ export default class Service {
   public async getTeamById(id: string | number) {
     return await ValorantTeams.getById(this.__auth, id);
   }
-  public async getResults() {
-    return await ValorantResults.get(this.__auth);
+  public async getResults(game: "valorant" | "lol") {
+    if(game === "valorant") {
+      return await ValorantResults.get(this.__auth);
+    }
+    else return await LOLResults.get(this.__auth);
   }
 }
---
+++ src/api/services/lol/MatchesService.ts
@@ -0,0 +1,12 @@
+import { MatchesData } from "../../../../types/index.js"
+
+export default class MatchesService {
+  public static async get(auth: string) {
+    const data = await (await fetch(process.env.API_URL + "/matches/lol", {
+      headers: {
+        authorization: auth
+      }
+    })).json();
+    return data as MatchesData[];
+  }
+}
