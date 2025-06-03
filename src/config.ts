e6f99c206557222592e68eb3013017d378f2f205
levisantosp <contatowizard01@gmail.com>
2025-06-02 22:56:51 -0300

    first steps for vlr simulator

---
+++ src/config.ts
@@ -0,0 +1,106 @@
+export const valorant_agents = [
+  {
+    name: "Brimstone",
+    role: "CONTROLLER"
+  },
+  {
+    name: "Omen",
+    role: "CONTROLLER"
+  },
+  {
+    name: "Viper",
+    role: "CONTROLLER"
+  },
+  {
+    name: "Raze",
+    role: "DUELIST"
+  },
+  {
+    name: "Cypher",
+    role: "SENTINEL"
+  },
+  {
+    name: "Sage",
+    role: "SENTINEL"
+  },
+  {
+    name: "Sova",
+    role: "INITIATOR"
+  },
+  {
+    name: "Phoenix",
+    role: "DUELIST"
+  },
+  {
+    name: "Jett",
+    role: "DUELIST"
+  },
+  {
+    name: "Breach",
+    role: "INITIATOR"
+  },
+  {
+    name: "Reyna",
+    role: "DUELIST"
+  },
+  {
+    name: "Killjoy",
+    role: "SENTINEL"
+  },
+  {
+    name: "Skye",
+    role: "INITIATOR"
+  },
+  {
+    name: "Yoru",
+    role: "DUELIST"
+  },
+  {
+    name: "Astra",
+    role: "CONTROLLER"
+  },
+  {
+    name: "KAY/O",
+    role: "INITIATOR"
+  },
+  {
+    name: "Chamber",
+    role: "SENTINEL"
+  },
+  {
+    name: "Neon",
+    role: "DUELIST"
+  },
+  {
+    name: "Fade",
+    role: "INITIATOR"
+  },
+  {
+    name: "Harbor",
+    role: "CONTROLLER"
+  },
+  {
+    name: "Gekko",
+    role: "INITIATOR"
+  },
+  {
+    name: "Deadlock",
+    role: "SENTINEL"
+  },
+  {
+    name: "Iso",
+    role: "DUELIST"
+  },
+  {
+    name: "Clove",
+    role: "CONTROLLER"
+  },
+  {
+    name: "Vyse",
+    role: "SENTINEL"
+  },
+  {
+    name: "Tejo",
+    role: "INITIATOR"
+  }
+] as const
