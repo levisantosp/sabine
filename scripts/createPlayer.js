a208540d4d38abea48cdc5b7ca66b00e2b785b93
levisantosp <contatowizard01@gmail.com>
2025-06-06 16:26:36 -0300

    replaced the splitter

--- scripts/createPlayer.js
+++ scripts/createPlayer.js
@@ -4,7 +4,7 @@ import getPlayers from "./getPlayers.js"
 export default function(data) {
   var players = getPlayers()
   var id = players.length + 1
-  var csv = data.map(row => `${id++};${row.join(";")}`).join("\n")
+  var csv = data.map(row => `${id++};${row.join(",")}`).join("\n")
   fs.appendFileSync("data.csv", `\n${csv}`, "utf-8")
   console.log("players added")
 }
