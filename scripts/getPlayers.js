45cf36fa685ec68bdceaa21a042bc3cd45364009
levisantosp <contatowizard01@gmail.com>
2025-06-06 15:58:21 -0300

    added scripts

---
+++ scripts/getPlayers.js
@@ -0,0 +1,20 @@
+import fs from "fs"
+
+export default function() {
+  const lines = fs.readFileSync("data.csv").toString().split("\n")
+  const headers = lines.shift().split(";")
+  const data = []
+  for(let i = 0; i < lines.length; i++) {
+    const obj = {}
+    const values = lines[i].split(";")
+    for(let i = 0; i < values.length; i++) {
+      var value = values[i]
+      if(!isNaN(value)) {
+        value = Number(values[i])
+      }
+      obj[headers[i]] = value
+    }
+    data.push(obj)
+  }
+  return data
+}
