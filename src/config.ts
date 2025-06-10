ab85f2aad17919a940f30fc124fb9edeb07a697d
levisantosp <contatowizard01@gmail.com>
2025-06-10 10:07:20 -0300

    added price

--- src/config.ts
+++ src/config.ts
@@ -106,36 +106,23 @@ export const valorant_agents = [
 ] as const
 
 export const valorant_weapons = [
-  // sidearms
-  "Classic",
-  "Shorty",
-  "Frenzy",
-  "Ghost",
-  "Sheriff",
-
-  // smg
-  "Stinger",
-  "Spectre",
-
-  // shotgun
-  "Bucky",
-  "Judge",
-
-  // rifle
-  "Bulldog",
-  "Guardian",
-  "Phantom",
-  "Vandal",
-
-  // sniper
-  "Marshal",
-  "Operator",
-  "Outlaw",
-
-  // heavy
-  "Ares",
-  "Odin",
-
-  // melee
-  "Melee"
+  { name: "Classic", price: 0 },
+  { name: "Shorty", price: 200 },
+  { name: "Frenzy", price: 450 },
+  { name: "Ghost", price: 500 },
+  { name: "Sheriff", price: 800 },
+  { name: "Stinger", price: 950 },
+  { name: "Spectre", price: 1600 },
+  { name: "Bucky", price: 850 },
+  { name: "Judge", price: 1850 },
+  { name: "Bulldog", price: 2050 },
+  { name: "Guardian", price: 2250 },
+  { name: "Phantom", price: 2900 },
+  { name: "Vandal", price: 2900 },
+  { name: "Marshal", price: 950 },
+  { name: "Operator", price: 4700 },
+  { name: "Outlaw", price: 2400 },
+  { name: "Ares", price: 1600 },
+  { name: "Odin", price: 3200 },
+  { name: "Melee", price: 0 }
 ] as const
