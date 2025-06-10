7932af802061a595d69545f544ade6362df9e442
levisantosp <contatowizard01@gmail.com>
2025-06-10 10:08:00 -0300

    minor changes

--- env.d.ts
+++ env.d.ts
@@ -1,18 +1,19 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      BOT_TOKEN: string;
-      MONGO_URI: string;
-      PREFIX: string;
-      ERROR_LOG: string;
-      COMMAND_LOG: string;
-      GUILDS_LOG: string;
-      SHARD_LOG: string;
-      INTERVAL: number;
-      USERS_LOG: string;
-      AUTH: string;
-      API_URL: string;
-      PORT: number;
+      BOT_TOKEN: string
+      MONGO_URI: string
+      ERROR_LOG: string
+      COMMAND_LOG: string
+      GUILDS_LOG: string
+      SHARD_LOG: string
+      INTERVAL: number
+      USERS_LOG: string
+      AUTH: string
+      API_URL: string
+      PORT: number
+      CDN_URL: string
+      DEVS: string[]
     }
   }
 }
