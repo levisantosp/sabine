589471415f91777983276d3c0bdf95f55ed63d30
levisantosp <contatowizard01@gmail.com>
2025-06-26 12:56:10 -0300

    added `/players` route

---
+++ src/server/routes/util/players.ts
@@ -0,0 +1,8 @@
+import type { FastifyInstance } from "fastify"
+import getPlayers from "../../../simulator/valorant/players/getPlayers.ts"
+
+export default function(fastify: FastifyInstance) {
+  fastify.get("/players", () => {
+    return getPlayers()
+  })
+}
