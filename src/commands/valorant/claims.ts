133051293add2baa0cd15d2ced33230ac9180fa5
levisantosp <contatowizard01@gmail.com>
2025-08-01 13:06:33 -0300

    create `/claims` command

---
+++ src/commands/valorant/claims.ts
@@ -0,0 +1,17 @@
+import createCommand from '../../structures/command/createCommand.ts'
+
+export default createCommand({
+  name: 'claims',
+  category: 'simulator',
+  description: 'See your claims counter',
+  descriptionLocalizations: {
+    'pt-BR': 'Veja o seu contador de obter'
+  },
+  userInstall: true,
+  async run({ ctx }) {
+    await ctx.reply('commands.claims.res', {
+      claims: ctx.db.user.claims,
+      pity: ctx.db.user.pity
+    })
+  }
+})
