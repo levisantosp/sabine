002702f9b34036f2a14ae31ad520ccc48ec30a83
levisantosp <contatowizard01@gmail.com>
2025-08-10 19:18:05 -0300

    create `/remind` command

---
+++ src/commands/misc/remind.ts
@@ -0,0 +1,23 @@
+import createCommand from '../../structures/command/createCommand.ts'
+
+export default createCommand({
+  name: 'remind',
+  nameLocalizations: {
+    'pt-BR': 'lembrar'
+  },
+  description: 'Notify you when you can run /claim again',
+  descriptionLocalizations: {
+    'pt-BR': 'Notifica você quando você puder usar /claim novamente'
+  },
+  category: 'misc',
+  async run({ ctx }) {
+    if(!ctx.db.user.remind) {
+      ctx.db.user.remind = true
+      await ctx.db.user.save()
+      return await ctx.reply('commands.remind.enabled')
+    }
+    ctx.db.user.remind = false
+    await ctx.db.user.save()
+    return await ctx.reply('commands.remind.disabled')
+  }
+})
