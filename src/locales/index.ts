18c3264c11ff28c4e45d67f303d66579ebdb8a7f
levisantosp <contatowizard01@gmail.com>
2025-08-10 19:16:27 -0300

    locales update

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -5,7 +5,7 @@ import * as Oceanic from 'oceanic.js'
 export type Args = {
   [key: string]: string | Error | number | Oceanic.File[] | undefined | null | bigint
 }
-export default function(lang: string, content: string, args?: Args): string {
+export default function t(lang: string, content: string, args?: Args): string {
   const path = resolve(`src/locales/${lang}.json`)
   const raw = readFileSync(path, 'utf-8')
   let json = JSON.parse(raw)
