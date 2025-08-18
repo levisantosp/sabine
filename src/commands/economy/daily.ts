e50080498b17f6c63758a9322db39438d6b6beb4
levisantosp <contatowizard01@gmail.com>
2025-08-17 23:24:02 -0300

    add fates to daily reward

--- src/commands/economy/daily.ts
+++ src/commands/economy/daily.ts
@@ -8,7 +8,7 @@ export default createCommand({
   },
   category: "economy",
   userInstall: true,
-  async run({ ctx, client }) {
+  async run({ ctx, client, t }) {
     if(
       ctx.db.user.daily_time &&
       ctx.db.user.daily_time.getTime() > Date.now()
@@ -16,15 +16,26 @@ export default createCommand({
       return await ctx.reply("commands.daily.has_been_claimed", { t: `<t:${((ctx.db.user.daily_time.getTime()) / 1000).toFixed(0)}:R>` })
     }
     let coins = BigInt(Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000)
-    await ctx.reply("commands.daily.res", { coins: coins.toLocaleString("en-US") })
+    let fates = Math.floor(Math.random() * (15 - 10 + 1)) + 10
     const member = client.guilds.get("1233965003850125433")!.members.get(ctx.interaction.user.id)
+    let content = t("commands.daily.res", {
+      coins: coins.toLocaleString("en-US"),
+      fates
+    }) + "\n"
     if(ctx.db.user.plan || member?.premiumSince) {
-      const bonus = BigInt(2500)
-      coins += bonus
-      await ctx.reply("commands.daily.bonus", { bonus: bonus.toLocaleString("en-us") })
+      const bonusCoins = coins * 10n
+      const bonusFates = fates + 5
+      coins += bonusCoins
+      fates += bonusFates
+      content += t("commands.daily.bonus", {
+        coins: bonusCoins.toLocaleString("en-us"),
+        fates: bonusFates
+      })
     }
     ctx.db.user.coins += coins
+    ctx.db.user.fates += fates
     ctx.db.user.daily_time = new Date(new Date().setHours(24, 0, 0, 0))
     await ctx.db.user.save()
+    await ctx.reply(content)
   }
 })
