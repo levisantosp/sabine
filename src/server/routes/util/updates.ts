b937a2217de7c82b3c16c27b2002d2f61719cf3c
levisantosp <contatowizard01@gmail.com>
2025-09-05 16:53:00 -0300

    minor changes

--- src/server/routes/util/updates.ts
+++ src/server/routes/util/updates.ts
@@ -2,10 +2,10 @@ import { PrismaClient } from "@prisma/client"
 import type { FastifyInstance } from "fastify"
 
 const prisma = new PrismaClient()
-const updates = await prisma.updates.findMany()
 
 export default async function(fastify: FastifyInstance) {
-  fastify.get("/updates", () => {
+  fastify.get("/updates", async() => {
+    const updates = await prisma.updates.findMany()
     return updates
   })
 }
