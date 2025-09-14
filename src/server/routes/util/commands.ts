dedffcce21ebb3db529823c6ed5806e6c21fb446
levisantosp <contatowizard01@gmail.com>
2025-09-13 23:03:07 -0300

    update commands on request

--- src/server/routes/util/commands.ts
+++ src/server/routes/util/commands.ts
@@ -7,24 +7,24 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default async function(fastify: FastifyInstance) {
-  const commands: any[] = []
-  for(const folder of readdirSync(path.resolve(__dirname, "../../../commands"))) {
-    for(const file of readdirSync(path.resolve(__dirname, `../../../commands/${folder}`))) {
-      const command = (await import(`../../../commands/${folder}/${file}`)).default.default ?? (await import(`../../../commands/${folder}/${file}`)).default
-      commands.push({
-        name: command.name,
-        nameLocalizations: command.nameLocalizations,
-        description: command.description,
-        descriptionLocalizations: command.descriptionLocalizations,
-        syntax: command.syntax,
-        syntaxes: command.syntaxes,
-        examples: command.examples,
-        permissions: command.permissions,
-        botPermissions: command.botPermissions
-      })
+  fastify.get("/commands", async() => {
+    const commands: any[] = []
+    for(const folder of readdirSync(path.resolve(__dirname, "../../../commands"))) {
+      for(const file of readdirSync(path.resolve(__dirname, `../../../commands/${folder}`))) {
+        const command = (await import(`../../../commands/${folder}/${file}`)).default
+        commands.push({
+          name: command.name,
+          nameLocalizations: command.nameLocalizations,
+          description: command.description,
+          descriptionLocalizations: command.descriptionLocalizations,
+          syntax: command.syntax,
+          syntaxes: command.syntaxes,
+          examples: command.examples,
+          permissions: command.permissions,
+          botPermissions: command.botPermissions
+        })
+      }
     }
-  }
-  fastify.get("/commands", () => {
     return commands
   })
 }
