645cefbdec0ac344ebedabc1fe6d51761eb3954a
levisantosp <contatowizard01@gmail.com>
2025-09-16 09:01:27 -0300

    include `content`

--- src/server/routes/util/updates.ts
+++ src/server/routes/util/updates.ts
@@ -5,7 +5,11 @@ const prisma = new PrismaClient()
 
 export default async function(fastify: FastifyInstance) {
   fastify.get("/updates", async() => {
-    const updates = await prisma.update.findMany()
+    const updates = await prisma.update.findMany({
+      include: {
+        content: true
+      }
+    })
     return updates
   })
 }
