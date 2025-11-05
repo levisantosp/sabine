618e298acfd59046fc14c67699885770fb8df776
levisantosp <contatowizard01@gmail.com>
2025-11-05 00:16:59 -0300

    fix(redis): clear all temporary keys on boot using scan

--- src/shard.ts
+++ src/shard.ts
@@ -1,7 +1,87 @@
 import { REST, Routes, ShardingManager } from 'discord.js'
+import Redis from 'redis'
 import { server } from './server/index.ts'
 import Logger from './util/Logger.ts'
 import EmbedBuilder from './structures/builders/EmbedBuilder.ts'
+import { prisma } from './database/prisma.ts'
+
+const redis: Redis.RedisClientType = Redis.createClient({
+  url: process.env.REDIS_URL
+})
+
+await redis.connect()
+
+const updateRedis = async() => {
+  const users = await prisma.user.findMany()
+  const blacklist = await prisma.blacklist.findMany()
+
+  await redis.set('blacklist', JSON.stringify(blacklist))
+
+  await redis.set(
+    'leaderboard:coins',
+    JSON.stringify(
+      {
+        updated_at: Date.now(),
+        data: users.map(user => ({
+          id: user.id,
+          coins: user.coins
+        }))
+          .filter(user => user.coins > 0)
+      },
+      (_, value) => typeof value === 'bigint' ? value.toString() : value
+    )
+  )
+
+  await redis.set(
+    'leaderboard:predictions',
+    JSON.stringify(
+      {
+        updated_at: Date.now(),
+        data: users.map(user => ({
+          id: user.id,
+          correct_predictions: user.correct_predictions
+        }))
+          .filter(user => user.correct_predictions > 0)
+      }
+    )
+  )
+
+  await redis.set(
+    'leaderboard:rating',
+    JSON.stringify(
+      {
+        updated_at: Date.now(),
+        data: users.map(user => ({
+          id: user.id,
+          elo: user.elo,
+          rank_rating: user.rank_rating
+        }))
+      }
+    )
+  )
+
+  setTimeout(updateRedis, 10 * 60 * 1000)
+}
+
+const patterns = ['*leaderboard:*', '*agent_selection:*', '*match:*']
+
+const keysToDelete = new Set<string>()
+
+for(const pattern of patterns) {
+  for await (const keys of redis.scanIterator({ MATCH: pattern, COUNT: 100 })) {
+    for(const key of keys) {
+      keysToDelete.add(key)
+    }
+  }  
+}
+
+const keys = [...keysToDelete]
+
+if(keys.length) {
+  await redis.del(keys)
+}
+
+await updateRedis()
 
 server.listen({
   host: process.env.HOST,
