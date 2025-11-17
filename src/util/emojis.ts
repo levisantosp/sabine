84db22ab8539978e8ee031b0508f122d1bc046e5
levisantosp <contatowizard01@gmail.com>
2025-11-17 11:21:12 -0300

    fix team emojis

--- src/util/emojis.ts
+++ src/util/emojis.ts
@@ -7,275 +7,276 @@ export type Emoji = {
 export const emojis: Emoji[] = [
   // DEFAULT TEAM EMOJIS
   {
-    emoji: '<:valorant_team:1349065037880365057>'
+    emoji: '<:team:1439977512074739713>'
   },
   {
-    emoji: '<:lol_team:1351581864468549784>'
+    emoji: '<:team:1439977512074739713>'
   },
   {
-    emoji: '<:cs_team:1354150393281511515>'
+    emoji: '<:team:1439977512074739713>'
   },
   // VCT CHINA
   {
     name: 'all gamers',
-    emoji: '<:all_gamers:1349065212023537794>'
+    emoji: '<:all_gamers:1439979628465750146>'
   },
   {
-    name: 'bilibili gaming',
-    emoji: '<:bilibili:1319735837969219758>'
+    name: 'guangzhou huadu bilibili gaming',
+    aliases: ['bilibili gaming'],
+    emoji: '<:blg:1439977527572435055>'
   },
   {
     name: 'edward gaming',
-    emoji: '<:edg:1319735623124516905>'
+    emoji: '<:edg:1439977531758612662>'
   },
   {
     name: 'funplus phoenix',
-    emoji: '<:fpx:1319735760374468618>'
+    emoji: '<:fpx:1439977528944230510>'
   },
   {
     name: 'jd mall jdg esports',
     aliases: ['jdg esports'],
-    emoji: '<:jdg:1349065661841936405>'
+    emoji: '<:jdg:1439977509255905310>'
   },
   {
     name: 'nova esports',
     aliases: ['nova esports gc'],
-    emoji: '<:nova:1349065823976820859>'
+    emoji: '<:nova:1439977507418935378>'
   },
   {
     name: 'trace esports',
     aliases: ['trace esports gc'],
-    emoji: '<:trace:1349065919833444383>'
+    emoji: '<:trace:1439977505258733671>'
   },
   {
     name: 'titan esports club',
-    emoji: '<:titan:1349066091812356210>'
+    emoji: '<:tec:1439977502876373074>'
   },
   {
     name: 'tyloo',
     aliases: ['tyloo gc'],
-    emoji: '<:tyloo:1349066156266356807>'
+    emoji: '<:tyloo:1439977501114765395>'
   },
   {
     name: 'wolves esports',
-    emoji: '<:Wolves:1349066242157318257>'
+    emoji: '<:wolves:1439977499890286643>'
   },
   {
     name: 'dragon ranger gaming',
-    emoji: '<:drg:1349066336365580299>'
+    emoji: '<:drg:1439977498598445107>'
   },
   {
     name: 'xi lai gaming',
-    emoji: '<:XLG:1349066428053196862>'
+    emoji: '<:xlg:1439977497088491684>'
   },
   // VCT AMERICAS
   {
     name: '100 thieves',
-    emoji: '<:100t:1319829320574832721>'
+    emoji: '<:100t:1439977516600262786>'
   },
   {
     name: 'cloud9',
-    emoji: '<:cloud9:1319829387545411677>',
+    emoji: '<:cloud9:1439977514973003888>',
     aliases: ['cloud9 kia']
   },
   {
     name: 'evil geniuses',
-    emoji: '<a:peepoEG:1349206142349213756>'
+    emoji: '<:eg:1439977513420984370>'
   },
   {
     name: 'furia',
-    emoji: '<:furia:1319734325838610594>',
+    emoji: '<:furia:1439977549374554283>',
     aliases: ['furia academy', 'furia esports']
   },
   {
     name: 'krü esports',
-    emoji: '<:kru:1319734403475181629>',
+    emoji: '<:kru:1439977547877322812>',
     aliases: ['kru blaze', 'krü blaze', 'visa krü']
   },
   {
     name: 'leviatán',
-    emoji: '<:leviatan:1319734466251325471>',
+    emoji: '<:leviatan:1439977545855406120>',
     aliases: ['leviatán academy', 'leviatán gc', 'leviatan esports']
   },
   {
     name: 'loud',
-    emoji: '<a:peepoLoud:1271960677988438038>',
+    emoji: '<:loud:1439971637226111016>',
     aliases: ['loud gc']
   },
   {
     name: 'mibr',
-    emoji: '<a:peepoMibr:1349206506352152688>',
+    emoji: '<:mibr:1439977463219486721>',
     aliases: ['mibr academy', 'mibr gc']
   },
   {
     name: 'nrg esports',
-    emoji: '<:nrg:1319734627153477673>',
+    emoji: '<:nrg:1439977543930482761>',
     aliases: ['nrg']
   },
   {
     name: 'sentinels',
-    emoji: '<a:peepoSen:1349205709253902489>',
+    emoji: '<:sentinels:1439977541703303391>',
     aliases: ['cubert academy', 'sentinels cubert academy']
   },
   {
     name: 'g2 esports',
     aliases: ['g2 gozen'],
-    emoji: '<:g2:1319734762973433998>'
+    emoji: '<:g2:1439977540193222776>'
   },
   {
     name: '2game esports',
     aliases: ['2game academy', '2game esports gc'],
-    emoji: '<:2game:1319734835379703888>'
+    emoji: '<:2g:1439977538096205834>'
   },
   {
     name: 'corinthians esports',
     aliases: ['corinthians esports inclusivo', 'corinthians e-sports', 'corinthians esports'],
-    emoji: '<:sccp_esports:1349204232862961745>'
+    emoji: '<:corinthians:1439977471859494912>'
   },
   {
     name: 'red canids',
-    emoji: '<a:red_canids:1349204671285170238>',
+    emoji: '<:red:1439977469925920899>',
     aliases: ['red kalunga', 'red academy']
   },
   {
     name: 'grêmio esports',
-    emoji: '<:gremio_esports:1349207265722368090>'
+    emoji: '<:gremio:1439977461868658718>'
   },
   {
     name: 'tbk esports',
-    emoji: '<:tbk:1349207626457546755>'
+    emoji: '<:tbk:1439977460346257468>'
   },
   {
     name: 'los grandes',
-    emoji: '<:los_grandes:1349207881068838913>',
+    emoji: '<:los:1439977458831986831>',
     aliases: ['los']
   },
   {
     name: 'envy',
-    emoji: '<:envy:1349208595329192030>'
+    emoji: '<:envy:1439977457326362744>'
   },
   {
     name: 'tsm',
-    emoji: '<:tsm:1349208771913580574>'
+    emoji: '<:tsm:1439977455971729520>'
   },
   {
     name: 'shopify rebellion',
     aliases: ['shopify rebellion gold', 'shopify rebellion black'],
-    emoji: '<:shopify:1349208960699203735>'
+    emoji: '<:shopify:1439977454369509448>'
   },
   {
     name: 'm80',
-    emoji: '<:m80:1351346019568193609>'
+    emoji: '<:m80:1439977452758630544>'
   },
   {
     name: 'fluxo w7m',
-    emoji: '<:fluxo_w7m:1366544710323404873>'
+    emoji: '<:fluxo_w7m:1439977448950468669>'
   },
   {
     name: 'vivo keyd stars',
-    emoji: '<:keyd:1366545502354804796>',
+    emoji: '<:keyd:1439977447415087244>',
     aliases: ['vivo keyd stars academy']
   },
   {
     name: 'pain gaming',
-    emoji: '<:pain:1366545677932433578>'
+    emoji: '<:pain:1439977445649289216>'
   },
   {
     name: 'flamengo esports',
-    emoji: '<:fla_esports:1369055007948673024>'
+    emoji: '<:flamengo:1439977443711520768>'
   },
   // VCT APAC
   {
     name: 'detonation focusme',
-    emoji: '<:DFM:1349073247911678046>',
+    emoji: '<:dfm:1439977495452454912>',
     aliases: ['detonation focusme gc']
   },
   {
     name: 'drx',
     aliases: ['drx changers', 'drx academy'],
-    emoji: '<:drx:1319735907284160512>'
+    emoji: '<:drx:1439977526028927097>'
   },
   {
     name: 'gen.g',
     aliases: ['gen.g global academy'],
-    emoji: '<:geng:1319735972027564194>'
+    emoji: '<:geng:1439977524590415892>'
   },
   {
     name: 'global esports',
-    emoji: '<:GE:1349074183531008152>',
+    emoji: '<:ge:1439977494064398440>',
     aliases: ['global esports academy']
   },
   {
     name: 'paper rex',
-    emoji: '<:prx:1319736078546243618>'
+    emoji: '<:prx:1439977522753310790>'
   },
   {
     name: 'rex regum qeon',
-    emoji: '<:rrq:1356243109331341353>'
+    emoji: '<:rrq:1439977450401693696>'
   },
   {
     name: 't1',
     aliases: ['t1 academy'],
-    emoji: '<:t1:1319736142551187498>'
+    emoji: '<:t1:1439977521176379543>'
   },
   {
     name: 'talon',
     aliases: ['talon academy'],
-    emoji: '<:talon:1349075084526092288>'
+    emoji: '<:talon:1439977491199561810>'
   },
   {
     name: 'team secret',
     aliases: ['team secret academy'],
-    emoji: '<:ts:1349075499456139295>'
+    emoji: '<:secret:1439977489983209712>'
   },
   {
     name: 'zeta division',
     aliases: ['zeta division academy'],
-    emoji: '<:zeta:1319736351385718874>'
+    emoji: '<:zeta:1439977519552925696>'
   },
   {
     name: 'nongshim redforce',
-    emoji: '<:nongshim_redforce:1349076108129337376>'
+    emoji: '<:nongshim:1439977488431316992>'
   },
   {
     name: 'boom esports',
-    emoji: '<:boom_esports:1349076374098415626>'
+    emoji: '<:boom:1439977486959116318>'
   },
   // VCT EMEA
   {
     name: 'bbl esports',
-    emoji: '<:BBL:1349076807000784907>'
+    emoji: '<:bbl:1439977485365153834>'
   },
   {
     name: 'fnatic',
-    emoji: '<:fnatic:1319735013922705501>'
+    emoji: '<:fnatic:1439977536355303537>'
   },
   {
     name: 'fut esports',
-    emoji: '<:fut:1319828037382045718>'
+    emoji: '<:fut:1439977518093434910>'
   },
   {
     name: 'giantx',
     aliases: ['giantx gc'],
-    emoji: '<:GX:1349078679568777307>'
+    emoji: '<:giantx:1439977483792547940>'
   },
   {
     name: 'karmine corp',
     aliases: ['karmine corp gc'],
-    emoji: '<:karmine:1349079127445082232>'
+    emoji: '<:karmine:1439977482320347166>'
   },
   {
     name: 'natus vincere',
-    emoji: '<:NAVI:1349079276343001201>'
+    emoji: '<:navi:1439977480839631003>'
   },
   {
     name: 'team heretics',
-    emoji: '<:heretics:1319735303438860370>'
+    emoji: '<:heretics:1439977534828838975>'
   },
   {
     name: 'koi',
     aliases: ['movistar koi', 'koi fénix'],
-    emoji: '<:koi:1349079921238085743>'
+    emoji: '<:koi:1439977479484735489>'
   },
   {
     name: 'team liquid',
@@ -284,15 +285,15 @@ export const emojis: Emoji[] = [
   },
   {
     name: 'team vitality',
-    emoji: '<:vitality:1349080749864915007>'
+    emoji: '<:liquid:1439977533293592726>'
   },
   {
     name: 'gentle mates',
-    emoji: '<:M8:1349080972645240873>'
+    emoji: '<:m8:1439977476313972818>'
   },
   {
     name: 'apeks',
-    emoji: '<:apeks:1349081350430265456>'
+    emoji: '<:apeks:1439977474900492418>'
   },
   {
     name: 'controller',
